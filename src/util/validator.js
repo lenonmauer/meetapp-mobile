adc7d688948766c664b2aee01f8bb560e7bf45af
Lenon Mauer <DATA52858@datainfo.inf.br>
2019-10-19 12:01:02 -0300

    add return in validator

--- src/util/validator.js
+++ src/util/validator.js
@@ -3,9 +3,11 @@ import ToastService from '~/services/toast';
 const validate = async (schema, form) => {
   try {
     await schema.validate(form);
+    return true;
   } catch (err) {
     const error = err.errors[0];
     ToastService.show(error, 2000);
+    return false;
   }
 };
 
